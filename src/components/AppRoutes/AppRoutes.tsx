c7158e24ca625ed20447ecb86f9a81a31970233a
MrKrodbon <tir25@ukr.net>
2025-06-22 20:32:58 +0300

    fix routes with gh-pages

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,15 +1,13 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
 import FormPage from "../../pages/FormPage/FormPage";
 
 const AppRoutes = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/submit" element={<FormPage />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/submit" element={<FormPage />} />
+    </Routes>
   );
 };
 
