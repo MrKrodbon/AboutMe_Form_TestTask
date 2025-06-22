884cc90e251e786a0d87ec93e932ba7cc02327c6
MrKrodbon <tir25@ukr.net>
2025-06-22 18:03:17 +0300

    fix vite config file path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "AboutMe_Form_TestTask/",
+  base: "/AboutMe_Form_TestTask/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
