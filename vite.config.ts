7bccfc2b1c38df8e3f87cf5d8be0e4fa8258bc52
MrKrodbon <tir25@ukr.net>
2025-06-22 14:38:01 +0300

    change path to base repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "AboutMe_Form_TestTask/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
